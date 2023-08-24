efe8863f68d81b555aa1513d31f6c4e55aa352e9
goodluck04 <47975507+goodluck04@users.noreply.github.com>
2023-08-24 13:22:11 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     images: {
-        domains: ["pbs.twimg.com", "i.ibb.co"]
+        // domains: ["pbs.twimg.com", "i.ibb.co"]
     }
 }
 
